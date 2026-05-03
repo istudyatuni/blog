d92211701314345f76dbe92a3539a83fe71b899b
Ilia <istudyatuni@gmail.com>
2026-05-03 20:09:06 +0300

    update typst and flake
    - nixpkgs: 2025-07-17 → 2026-05-01

--- flake.nix
+++ flake.nix
@@ -18,22 +18,7 @@
       in {
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            (typst.overrideAttrs rec {
-              version = "0.13.1-dev";
-              # do not forget to update workflows/pages.yaml
-              src = pkgs.fetchFromGitHub {
-                owner = "typst";
-                repo = "typst";
-                rev = "b790c6d59ceaf7a809cc24b60c1f1509807470e2";
-                hash = "sha256-+0Gye1zwBQ0rGgX/3tM7hrOaH66peo2ssjc6xNTO84M=";
-              };
-              cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-                inherit version src;
-                pname = "typst";
-                hash = "sha256-fWi7rPh3BYrqRHoWhCE3SgR+bETfFZlJYCGRbASJhzk=";
-              };
-              doCheck = false;
-            })
+            typst
             static-web-server
           ];
           shellHook = ''
