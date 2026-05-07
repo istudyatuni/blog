a44a8ca3ee97227602b708d44c99963ee8546f48
Ilia <istudyatuni@gmail.com>
2026-05-07 14:45:46 +0300

    use dev typst

--- flake.nix
+++ flake.nix
@@ -18,7 +18,30 @@
       in {
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            typst
+            (let
+              rev = "86e38439b61cd31c002a7ae5951c947e8630eeae";
+            in
+              typst.overrideAttrs rec {
+                version = "0.14.2-dev";
+                # do not forget to update workflows/pages.yaml
+                src = pkgs.fetchFromGitHub {
+                  owner = "typst";
+                  repo = "typst";
+                  inherit rev;
+                  hash = "sha256-QqIr1HykzRx0UDUg+PWehQNDE/XSZyE6TRfl/Kn0JZc=";
+                };
+                cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+                  inherit version src;
+                  pname = "typst";
+                  hash = "sha256-JNBYgk6B31TM0EUw6/6WBmMaYws9YYt3Hwvq8kds8Ak=";
+                };
+                postPatch = '''';
+                preBuild = ''
+                  export TYPST_COMMIT_SHA="${rev}"
+                '';
+                doCheck = false;
+                doInstallCheck = false;
+              })
             static-web-server
           ];
           shellHook = ''
